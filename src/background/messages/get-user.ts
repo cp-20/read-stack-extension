be8553ca85a1d16ce879d344db9e5769b2a57439
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-20 02:54:55 +0900

    動くように修正

--- src/background/messages/get-user.ts
+++ src/background/messages/get-user.ts
@@ -3,8 +3,22 @@ import type { AuthUser } from '@supabase/supabase-js';
 import type { PlasmoMessaging } from '@plasmohq/messaging';
 
 import { storage } from '@/background/messages/init-session';
+import { supabase } from '@/lib/boundaries/supabase';
 
-export const getUser = () => storage.get<AuthUser>('user');
+export const getUser = async () => {
+  const user = await storage.get<AuthUser>('user');
+  if (user !== undefined) {
+    return user;
+  }
+
+  const userRes = await supabase.auth.getUser();
+  if (userRes.error) {
+    throw userRes.error;
+  }
+
+  storage.set('user', userRes.data.user);
+  return userRes.data.user;
+};
 
 const handler: PlasmoMessaging.MessageHandler<
   void,
